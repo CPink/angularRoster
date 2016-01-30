fd47ee24c7f428d5a3bda6ba6309602cf7851c20
CPink <cepinkerton.85@gmail.com>
2016-01-29 23:58:26 -0700

    added a login to auth and save users to firebase

--- app/app.js
+++ app/app.js
@@ -7,29 +7,35 @@
             'ionic',
             'ui.router',
             'firebase',
-            'angularfire'
-        ])
+            ])
 
-    .run(function ($ionicPlatform) {
-        $ionicPlatform.ready(function () {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
-            if (window.StatusBar) {
-                StatusBar.styleDefault();
-            }
-        });
-    })
+    // .run(function ($ionicPlatform) {
+    //     $ionicPlatform.ready(function () {
+    //         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    //         // for form inputs)
+    //         if (window.cordova && window.cordova.plugins.Keyboard) {
+    //             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    //         }
+    //         if (window.StatusBar) {
+    //             StatusBar.styleDefault();
+    //         }
+    //     });
+    // })
 
 
         .constant('FirebaseUrl', 'https://nflroster.firebaseio.com/')
 
         .config(function ($urlRouterProvider, $stateProvider) {
             $stateProvider
-                .state('main', {
+                .state('login', {
                     url: '/',
+                    controller: 'loginController',
+                    controllerAs: 'loginCtrl',
+                    templateUrl: 'app/components/login/login.html'
+                })
+                
+                .state('main', {
+                    url: '/main',
                     controller: 'mainController',
                     controllerAs: 'mainCtrl',
                     templateUrl: 'app/components/main/main.html'
